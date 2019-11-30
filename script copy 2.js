be1d49c56ff2829f448cbb6818583f05cb14e1ea
Arauco <arishel@gmail.com>
2019-11-30 12:12:06 -0800

    changed input method from alerts to HTML form

--- script copy 2.js
+++ script copy 2.js
@@ -16,30 +16,45 @@ function generatePassword() {
 
     // Set variables used in while loop
     var userCharList = [];
+    var passwordArray = [];
     var passLength = 0;
     var passLowerCase = false;
     var passUpperCase = false;
     var passSpecial = false;
     var passNum = false;
 
-    var passwordArray = [];
 
-    // while (
+    // Retrieve form input from index.html
+    var passLength = document.getElementById("checkLength").value;
+    var passLowerCase = document.getElementById("checkLowerCase").checked;
+    var passUpperCase = document.getElementById("checkUpperCase").checked;
+    var passNum = document.getElementById("checkNum").checked;
+    var passSpecial = document.getElementById("checkSpecial").checked;
+    console.log("passLength: " + passLength);
+    console.log("passLowerCase: " + passLowerCase);
+    console.log("passUpperCase: " + passUpperCase);
+    console.log("passSpecial: " + passSpecial);
+    console.log("passNum: " + passNum);
+    // user choose character types to be used in the 
 
-    while (passLength < 8 ||
+
+
+    if (passLength < 8 ||
         passLength > 128) {
 
         // ask user password length between 8 and 128
-        var passLength = prompt("Choose password length with value of 8 to 128");
+        alert("Choose password length with value of 8 to 128");
+        console.log(passLength);
     }
-    console.log(passLength);
 
-    while (passLowerCase === false &&
+    else if (passLowerCase === false &&
         passUpperCase === false &&
         passSpecial === false &&
         passNum === false) {
+        alert("please select at least one character type")
+    } else {
         // ask user uppercase letters
-        var passLowerCase = confirm("Would you like to use lowercase letters?");
+        // var passLowerCase = confirm("Would you like to use lowercase letters?");
         if (passLowerCase === true) {
             for (let i = 0; i < lowerCase.length; i++) {
                 userCharList.push(lowerCase[i])
@@ -47,7 +62,7 @@ function generatePassword() {
         }
         console.log(passLowerCase);
         // ask user uppercase letters
-        var passUpperCase = confirm("Would you like to use uppercase letters?");
+        // var passUpperCase = confirm("Would you like to use uppercase letters?");
         if (passUpperCase === true) {
             for (let i = 0; i < upperCase.length; i++) {
                 userCharList.push(upperCase[i])
@@ -55,7 +70,7 @@ function generatePassword() {
         }
         console.log(passUpperCase);
         // ask user numbers
-        var passNum = confirm("Would you like to use numbers?");
+        // var passNum = confirm("Would you like to use numbers?");
         if (passNum === true) {
             for (let i = 0; i < numbers.length; i++) {
                 userCharList.push(numbers[i])
@@ -63,29 +78,15 @@ function generatePassword() {
         }
         console.log(passNum);
         // ask user special characters
-        var passSpecial = confirm("Would you like to use special characters?");
+        // var passSpecial = confirm("Would you like to use special characters?");
         if (passSpecial === true) {
             for (let i = 0; i < symbols.length; i++) {
                 userCharList.push(symbols[i])
             }
         }
         console.log(passSpecial);
-    }
-
-    // Retrieve form input from index.html
-    //    var passLength = document.getElementById("#passLength");
-    //    var passLowerCase = document.getElementById("#passLowerCase");
-    //    var passUpperCase = document.getElementById("#passUpperCase");
-    //    var passNum = document.getElementById("#passNum");
-    //    var passSpecial = document.getElementById("#passSpecial");
-    // console.log("passLength: " + passLength.valueOf()); 
-    // console.log("userCharList: " + userCharList);
-    // console.log("passLowerCase: " + passLowerCase);
-    // console.log("passUpperCase: " + passUpperCase);
-    // console.log("passSpecial: " + passSpecial);
-    // console.log("passNum: " + passNum);
-    // user choose character types to be used in the 
-
+    
+    console.log("userCharList: " + userCharList);
     for (let i = 0; i < passLength.valueOf(-1); i++) {
         passwordArray.push(userCharList[Math.floor(Math.random() * userCharList.length)]);
     }
@@ -94,9 +95,7 @@ function generatePassword() {
     var password = passwordArray.join("");
     console.log(password);
     return password;
-
-    // return password;
-
+    }
 }
 
 
